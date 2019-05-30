72d34c861e6de9291dd03e75be254e7a33c6277b
naporin0624 <naporin0624@gmail.com>
2019-05-31 03:27:40 +0900

    a

--- src/views/FaceEmotionDemo.vue
+++ src/views/FaceEmotionDemo.vue
@@ -14,13 +14,13 @@
           <canvas ref="image" style="display: none"></canvas>
           <v-container>
             <v-layout wrap align-center justify-center row fill-height>
-              <v-flex md4 v-for="(img1, idx) in personImage" :key="img1">
-                <img
-                  :src="img1"
+              <v-flex md4 v-for="(imgsrc, idx) in personImage" :key="idx">
+                <v-img
+                  :src="imgsrc"
                   width="160"
                   height="160"
                   style="margin: auto; transform: scaleX(-1);"
-                  :style="{ 'border': 'solid 5px ' + colorList[idx] }"
+                  :style="{ border: 'solid 5px ' + colorList[idx] }"
                 />
               </v-flex>
             </v-layout>
@@ -57,7 +57,6 @@ import RaderChart from "@/components/RaderChart.vue";
   }
 })
 export default class FaceEmotionDemo extends Vue {
-  public predicts?: Prediction[];
   public personImage: string[] = [];
   public isFaceRecognitionStart: boolean = false;
   public colorList: string[] = [
@@ -105,12 +104,11 @@ export default class FaceEmotionDemo extends Vue {
     const stream = await deviceNav;
     const video = <HTMLVideoElement>this.$refs.video;
     const videoBox = <HTMLVideoElement>this.$refs.videobox;
-    if (video && videoBox) {
-      video.srcObject = stream;
-      video.width = videoBox.clientWidth;
-    }
+    video.srcObject = stream;
+    video.width = videoBox.clientWidth;
   }
 
+  // btnAction
   public toggleFaceEmotionRecognition(): void {
     if (this.isFaceRecognitionStart) {
       this.isFaceRecognitionStart = false;
@@ -129,99 +127,94 @@ export default class FaceEmotionDemo extends Vue {
         canvas.width = video.clientWidth;
         canvas.height = video.clientHeight;
         const context = <CanvasRenderingContext2D>canvas.getContext("2d");
-        if (context) {
-          // 画像取得
-          context.drawImage(video, 0, 0, canvas.width, canvas.height);
-          const imageURL: string = canvas.toDataURL("image/png");
-          const base64: string = imageURL.split(",")[1];
-          const buffer: ArrayBuffer = base64ToBuffer(base64);
-          // APIのリソース生成
-          const blob: Blob = new Blob([buffer], { type: "image/png" });
-          // API叩く
-          const predicts: Prediction[] = await faceEmotionDetector(blob);
+        // 画像取得
+        context.drawImage(video, 0, 0, canvas.width, canvas.height);
+        const imageURL: string = canvas.toDataURL("image/png");
+        const base64: string = imageURL.split(",")[1];
+        const buffer: ArrayBuffer = base64ToBuffer(base64);
+        // APIのリソース生成
+        const blob: Blob = new Blob([buffer], { type: "image/png" });
+        // API叩く
+        const predicts: Prediction[] = await faceEmotionDetector(blob);
+        // Chartを作るデータの作成
+        const datasets: Chart.ChartDataSets[] = predicts.map(
+          (predict: Prediction, idx: number) => {
+            return {
+              label: `Person${idx + 1}`,
+              backgroundColor: color(this.colorList[idx])
+                .alpha(0.5)
+                .rgbString(),
+              borderColor: color(this.colorList[idx]).rgbString(),
+              pointBackgroundColor: color(this.colorList[idx]).rgbString(),
+              data: predict.emotion_predictions
+                .sort((a: any, b: any) => {
+                  return a.label_id - b.label_id;
+                })
+                .map((a: any) => {
+                  return a.probability;
+                })
+            };
+          }
+        );
 
-          // Chartを作るデータの作成
-          const datasets: Chart.ChartDataSets[] = predicts.map(
-            (predict: Prediction, index: number) => {
-              return {
-                label: `Person${index}`,
-                backgroundColor: color(this.colorList[index])
-                  .alpha(0.5)
-                  .rgbString(),
-                borderColor: color(this.colorList[index]).rgbString(),
-                pointBackgroundColor: color(this.colorList[index]).rgbString(),
-                data: predict.emotion_predictions
-                  .sort((a: any, b: any) => {
-                    return a.label_id - b.label_id;
-                  })
-                  .map((a: any) => {
-                    return a.probability;
-                  })
+        // データの更新
+        this.chartData = {
+          labels: [
+            "neutral",
+            "happiness",
+            "surprise",
+            "sadness",
+            "anger",
+            "disgest",
+            "fear",
+            "contempt"
+          ],
+          datasets
+        };
+        // ymin, xmin, ymax, xmax
+
+        this.personImage = await Promise.all(
+          predicts.map(async (predict: Prediction, idx: number) => {
+            const draw = <HTMLCanvasElement>this.$refs.draw;
+            const imageCanvas = <HTMLCanvasElement>this.$refs.image;
+
+            const image = new Image();
+            image.src = imageURL;
+
+            function imageOnload() {
+              image.onload = () => {
+                return Promise.resolve(true);
               };
             }
-          );
+            // 画像が読み込まれるまで待つ
+            await imageOnload();
 
-          // データの更新
-          this.chartData = {
-            labels: [
-              "neutral",
-              "happiness",
-              "surprise",
-              "sadness",
-              "anger",
-              "disgest",
-              "fear",
-              "contempt"
-            ],
-            datasets
-          };
-          // ymin, xmin, ymax, xmax
+            draw.width = image.width;
+            draw.height = image.height;
+            const ctx = <CanvasRenderingContext2D>draw.getContext("2d");
 
-          this.personImage = await Promise.all(
-            predicts.map(async (predict: Prediction, idx: number) => {
-              const draw = <HTMLCanvasElement>this.$refs.draw;
-              const imageCanvas = <HTMLCanvasElement>this.$refs.image;
+            const ymin: number = canvas.height * predict.detection_box[0] - 25;
+            const xmin: number = canvas.width * predict.detection_box[1] - 25;
+            const ymax: number = canvas.height * predict.detection_box[2] + 25;
+            const xmax: number = canvas.width * predict.detection_box[3] + 25;
 
-              const image = new Image();
-              image.src = imageURL;
+            ctx.drawImage(image, 0, 0);
+            const image1 = ctx.getImageData(
+              xmin,
+              ymin,
+              xmax - xmin,
+              ymax - ymin
+            );
+            imageCanvas.width = image1.width;
+            imageCanvas.height = image1.height;
+            const ctxImage = <CanvasRenderingContext2D>(
+              imageCanvas.getContext("2d")
+            );
+            ctxImage.putImageData(image1, 0, 0);
 
-              function imageOnload() {
-                image.onload = () => {
-                  return Promise.resolve(true);
-                };
-              }
-              // 画像が読み込まれるまで待つ
-              await imageOnload();
-
-              draw.width = image.width;
-              draw.height = image.height;
-              const ctx = <CanvasRenderingContext2D>draw.getContext("2d");
-
-              const ymin: number =
-                canvas.height * predict.detection_box[0] - 25;
-              const xmin: number = canvas.width * predict.detection_box[1] - 25;
-              const ymax: number =
-                canvas.height * predict.detection_box[2] + 25;
-              const xmax: number = canvas.width * predict.detection_box[3] + 25;
-
-              ctx.drawImage(image, 0, 0);
-              const image1 = ctx.getImageData(
-                xmin,
-                ymin,
-                xmax - xmin,
-                ymax - ymin
-              );
-              imageCanvas.width = image1.width;
-              imageCanvas.height = image1.height;
-              const ctxImage = <CanvasRenderingContext2D>(
-                imageCanvas.getContext("2d")
-              );
-              ctxImage.putImageData(image1, 0, 0);
-
-              return Promise.resolve(imageCanvas.toDataURL("image/png"));
-            })
-          );
-        }
+            return Promise.resolve(imageCanvas.toDataURL("image/png"));
+          })
+        );
       }
     }, 1000);
   }
